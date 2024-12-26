57234228576cc8e255eb95a51a4d4042a6a40fec
sodapi <rafyrachmansyahh@gmail.com>
2024-12-26 19:34:22 +0700

    fix/add-user-data

--- src/components/Utils/Welcome.jsx
+++ src/components/Utils/Welcome.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Avatar from "../../assets/ProfilePhoto.png";
 
 export default function Welcome() {
+  const { profile } = useSelector((state) => state.auth);
+
+  const firstName = profile?.first_name || "Guest";
+  const lastName = profile?.last_name || "";
+
+  // Fallback to default Avatar if profile_image is null or matches the placeholder
+  const profileImage =
+    profile?.profile_image ===
+    "https://minio.nutech-integrasi.com/take-home-test/null"
+      ? Avatar
+      : profile?.profile_image || Avatar;
+
   return (
-    <div className="flex flex-col gap-5 h-[100px]">
-      <img src={Avatar} className="w-[75px]" />
-      <div>
-        <p className="text-[18px]">Selamat Datang,</p>
-        <p className="font-semibold text-[36px]">Kristanto Wibowo</p>
+    <div className="flex flex-col items-center gap-3 h-auto">
+      <img
+        src={profileImage}
+        className="w-20 h-20 rounded-full object-cover"
+        alt="User Avatar"
+      />
+      <div className="text-center">
+        <p className="text-lg text-gray-600">Selamat Datang,</p>
+        <p className="font-bold text-2xl">
+          {firstName} {lastName}
+        </p>
       </div>
     </div>
   );
