62de3d6a1ba00a57f4e3490d60c732de521739b0
sodapi <rafyrachmansyahh@gmail.com>
2024-12-26 19:32:43 +0700

    fix/add-protected-route

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,59 @@ import Profile from "./pages/Profile";
 import History from "./pages/Service/History";
 import Payment from "./pages/Service/Payment";
 import Topup from "./pages/Service/Topup";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
 function App() {
   return (
-    <div className="App ">
+    <div className="App">
       <BrowserRouter>
         <Routes>
-          {/* Global */}
+          {/* Public Routes */}
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/top-up" element={<Topup />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/transaction" element={<History />} />
-          <Route path="/akun" element={<Profile />} />
+
+          {/* Protected Routes */}
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/top-up"
+            element={
+              <ProtectedRoute>
+                <Topup />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/payment"
+            element={
+              <ProtectedRoute>
+                <Payment />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/transaction"
+            element={
+              <ProtectedRoute>
+                <History />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/akun"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/*" element={<Login />} />
         </Routes>
       </BrowserRouter>
