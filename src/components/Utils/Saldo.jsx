d794ed8146c0bc5035f576b70c736b33175640da
sodapi <rafyrachmansyahh@gmail.com>
2024-12-26 21:42:48 +0700

    fix/saldo-api-get

--- src/components/Utils/Saldo.jsx
+++ src/components/Utils/Saldo.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBalance } from "../../auth/authSlice";
 import BG from "../../assets/BackgroundSaldo.png";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 export default function Saldo() {
   const [saldoVisible, setSaldoVisible] = useState(false);
+  const dispatch = useDispatch();
+  const { balance, isLoading, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    dispatch(getBalance());
+  }, [dispatch]);
 
   const toggleSaldoVisibility = () => {
     setSaldoVisible(!saldoVisible);
@@ -23,7 +33,13 @@ export default function Saldo() {
         <div className="flex flex-row items-center gap-2 mt-2">
           <span className="text-[36px] font-bold">Rp</span>
           <span className="text-[36px] font-bold">
-            {saldoVisible ? "100.000" : "•••••••"}
+            {isLoading
+              ? "Loading..."
+              : isError
+              ? "Error"
+              : saldoVisible
+              ? balance?.toLocaleString() || "0"
+              : "•••••••"}
           </span>
         </div>
         {/* Toggle Button */}
@@ -33,6 +49,7 @@ export default function Saldo() {
           {saldoVisible ? "Tutup Saldo" : "Lihat Saldo"}
           {saldoVisible ? <FaRegEyeSlash /> : <FaRegEye />}
         </button>
+        {isError && <p className="text-sm text-red-500 mt-2">{message}</p>}
       </div>
     </div>
   );
