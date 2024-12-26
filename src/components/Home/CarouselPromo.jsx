2a0cd421403b29d86f8fc2e6aed3d5380d2d00a0
sodapi <rafyrachmansyahh@gmail.com>
2024-12-26 21:42:30 +0700

    fix/carousel-promo-api-get

--- src/components/Home/CarouselPromo.jsx
+++ src/components/Home/CarouselPromo.jsx
@@ -1,20 +1,38 @@
-import React from "react";
-import Banner1 from "../../assets/Banner1.png";
-import Banner2 from "../../assets/Banner2.png";
-import Banner3 from "../../assets/Banner3.png";
-import Banner4 from "../../assets/Banner4.png";
-import Banner5 from "../../assets/Banner5.png";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBanners } from "../../auth/authSlice";
 
-export default function CarouselServices() {
+export default function CarouselPromo() {
+  const dispatch = useDispatch();
+  const { banners, isLoading, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  // Fetch banners on component mount
+  useEffect(() => {
+    dispatch(getBanners());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>{message}</div>;
+  }
+
   return (
     <div className="flex flex-col gap-5 pl-[150px]">
       <h1 className="font-medium">Temukan promo menarik</h1>
       <div className="flex flex-row gap-5 overflow-x-auto scrollbar-hidden">
-        <img src={Banner1} className="w-[325px] flex-shrink-0" />
-        <img src={Banner2} className="w-[325px] flex-shrink-0" />
-        <img src={Banner3} className="w-[325px] flex-shrink-0" />
-        <img src={Banner4} className="w-[325px] flex-shrink-0" />
-        <img src={Banner5} className="w-[325px] flex-shrink-0" />
+        {banners?.map((banner, index) => (
+          <img
+            key={index}
+            src={banner.banner_image}
+            alt={banner.banner_name}
+            className="w-[325px] flex-shrink-0"
+          />
+        ))}
       </div>
     </div>
   );
